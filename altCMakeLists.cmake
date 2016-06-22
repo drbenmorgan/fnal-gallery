bac2f0a43f3365d6226bdbccfae6a0b9dbaa45c2
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-06-22 19:00:44 +0100

    Implement basic "altCMake" structure
    Following work on canvas, implement build of gallery in the "altCMake"
    style. Provide "altCMakeLists.cmake" scripts mirroring main Cmake
    scripts, using these via an "ALT_CMAKE" boolean switch in the main
    CMakeLists.txt scripts.
    
    Implement expected basic structure of modern cmake structures, but
    needs further work in canvas to ensure all needed modules and
    libraries are available.

---
+++ altCMakeLists.cmake
@@ -0,0 +1,51 @@
+# ======================================================================
+#  gallery main build file
+#
+#  cd .../path/to/build/directory
+#  cmake [-DCMAKE_INSTALL_PREFIX=/install/path] <pathtosource>
+#  make
+#  make test
+#  make install
+#  make package (builds distribution tarfile)
+# ======================================================================
+
+# - Project setup
+cmake_minimum_required(VERSION 3.3)
+project(gallery VERSION 1.3.2)
+
+#-----------------------------------------------------------------------
+# Standard and Custom CMake Modules
+#
+# - Cetbuildtools, version2
+find_package(cetbuildtools2 0.1.0 REQUIRED)
+list(INSERT CMAKE_MODULE_PATH 0 ${cetbuildtools2_MODULE_PATH})
+include(CetInstallDirs)
+include(CetCMakeSettings)
+include(CetCompilerSettings)
+include(CetTest)
+
+# C++ Standard Config
+set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(canvas_COMPILE_FEATURES
+  cxx_auto_type
+  cxx_generic_lambdas
+  )
+
+# these are minimum required versions, not the actual product versions
+find_package(canvas REQUIRED)
+find_package(cetlib REQUIRED)
+find_package(ROOT 6.0.0 REQUIRED)
+
+# art_dictionary
+include(ArtDictionary)
+
+# source
+add_subdirectory(gallery)
+
+# ups - table and config files
+#add_subdirectory(ups)
+
+# packaging utility
+#include(UseCPack)
---
+++ gallery/altCMakeLists.cmake
@@ -0,0 +1,56 @@
+include(ArtDictionary)
+
+set(gallery_SOURCES
+  AssnsBranchData.cc
+  AssnsBranchData.h
+  BranchData.cc
+  BranchData.h
+  BranchMapReader.cc
+  BranchMapReader.h
+  DataGetterHelper.cc
+  DataGetterHelper.h
+  Event.cc
+  Event.h
+  EventHistoryGetter.cc
+  EventHistoryGetter.h
+  EventNavigator.cc
+  EventNavigator.h
+  FindMaker.cc
+  FindMaker.h
+  Handle.cc
+  Handle.h
+  HistoryGetterBase.cc
+  HistoryGetterBase.h
+  TypeLabelInstanceKey.h
+  ValidHandle.cc
+  ValidHandle.h
+  throwFunctions.cc
+  throwFunctions.h
+  )
+
+add_library(gallery SHARED ${gallery_SOURCES})
+target_include_directories(gallery
+  PUBLIC
+   $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}>
+   $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
+   ${ROOT_INCLUDE_DIRS}
+   )
+target_link_libraries(gallery
+  PUBLIC
+    canvas::canvas_Persistency_Common
+    canvas::canvas_Persistency_Provenance
+    canvas::canvas_Utilities
+    cetlib::cetlib
+    ${ROOT_Core_LIBRARY}
+    ${ROOT_RIO_LIBRARY}
+    ${ROOT_Tree_LIBRARY}
+    )
+
+
+# Must include dirs as art/build_dictionary don't understand genexs yet
+include_directories(${PROJECT_SOURCE_DIR})
+include_directories(${cetlib_INCLUDEDIR})
+include_directories(${ROOT_INCLUDE_DIRS})
+art_dictionary(DICTIONARY_LIBRARIES gallery)
+
+
