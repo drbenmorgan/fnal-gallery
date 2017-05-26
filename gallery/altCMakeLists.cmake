8fa014a71075ed9e5e4934908a058dec87d66ae2
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2017-05-26 11:22:31 +0100

    Add install of support files

--- gallery/altCMakeLists.cmake
+++ gallery/altCMakeLists.cmake
@@ -45,6 +45,7 @@ install(TARGETS gallery
   EXPORT ${PROJECT_NAME}Targets
   DESTINATION ${CMAKE_INSTALL_LIBDIR}
   )
+
 install(FILES
   AssnsBranchData.h
   BranchData.h
@@ -72,4 +73,44 @@ art_dictionary(DICTIONARY_LIBRARIES gallery)
 if(BUILD_TESTING)
   add_subdirectory(test)
 endif()
+
+# Support files
+#-----------------------------------------------------------------------
+# Create exports file(s)
+include(CMakePackageConfigHelpers)
+
+# - Common to both trees
+write_basic_package_version_file(
+  "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+  COMPATIBILITY SameMajorVersion
+  )
+
+# - Build tree (EXPORT only for now, config file needs some thought,
+#   dependent on the use of multiconfig)
+export(
+  EXPORT ${PROJECT_NAME}Targets
+  NAMESPACE ${PROJECT_NAME}::
+  FILE "${PROJECT_BINARY_DIR}/${PROJECT_NAME}Targets.cmake"
+  )
+
+# - Install tree
+configure_package_config_file("${PROJECT_SOURCE_DIR}/${PROJECT_NAME}Config.cmake.in"
+  "${PROJECT_BINARY_DIR}/InstallCMakeFiles/${PROJECT_NAME}Config.cmake"
+  INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+  PATH_VARS CMAKE_INSTALL_INCLUDEDIR
+  )
+
+install(
+  FILES
+    "${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake"
+    "${PROJECT_BINARY_DIR}/InstallCMakeFiles/${PROJECT_NAME}Config.cmake"
+  DESTINATION
+    "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+    )
+
+install(
+  EXPORT ${PROJECT_NAME}Targets
+  NAMESPACE ${PROJECT_NAME}::
+  DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+  )
 
